e3a5aa649ad8a37d2988a5c0fc9378ed5af67e27
sderev <contact@sderev.com>
2023-07-07 14:05:32 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# windows-screenshot-for-linux
+# Windows Screenshot for Linux
+
 A tool that automates the transfer and management of screenshots from Windows to a Linux VM, providing easy access and integration in your text editor.
---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+
+VERSION = "0.0.0"
+
+
+def read_requirements():
+    with open("requirements.txt") as file:
+        return list(file)
+
+
+def get_long_description():
+    with open("README.md", encoding="utf8") as file:
+        return file.read()
+
+
+setup(
+    name="wslshot",
+    description="",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    author="Sébastien De Revière",
+    url="https://github.com/sderev/wslshot",
+    project_urls={
+        "Documentation": "https://github.com/sderev/wslshot",
+        "Issues": "http://github.com/sderev/wslshot/issues",
+        "Changelog": "https://github.com/sderev/wslshot/releases",
+    },
+    license="Apache Licence, Version 2.0",
+    version=VERSION,
+    packages=find_packages(),
+    install_requires=read_requirements(),
+    entry_points={
+        "console_scripts": [
+        ]
+    },
+    python_requires=">=3.8",
+)
